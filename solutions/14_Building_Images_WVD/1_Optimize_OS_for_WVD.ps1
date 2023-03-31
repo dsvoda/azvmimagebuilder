95304d3eeade27c9e22c4b3308f031146221a62b
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2023-03-31 16:52:30 -0500

    Update 1_Optimize_OS_for_WVD.ps1

--- solutions/14_Building_Images_WVD/1_Optimize_OS_for_WVD.ps1
+++ solutions/14_Building_Images_WVD/1_Optimize_OS_for_WVD.ps1
@@ -5,7 +5,7 @@
  New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
  $LocalPath = $drive + '\' + $appName 
  set-Location $LocalPath
- $osOptURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
+ $osOptURL = 'https://github.com/dsvoda/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
  $osOptURLexe = 'Windows_10_VDI_Optimize-master.zip'
  $outputPath = $LocalPath + '\' + $osOptURLexe
  Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
@@ -15,7 +15,7 @@
  Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-master
  
  # instrumentation
- $osOptURL = 'https://raw.githubusercontent.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/master/Win10_VirtualDesktop_Optimize.ps1'
+ $osOptURL = 'https://raw.githubusercontent.com/dsvoda/Virtual-Desktop-Optimization-Tool/main/Windows_VDOT.ps1'
  $osOptURLexe = 'optimize.ps1'
  Invoke-WebRequest -Uri $osOptURL -OutFile $osOptURLexe
  
@@ -23,7 +23,7 @@
  
  # Patch: overide the Win10_VirtualDesktop_Optimize.ps1 - setting 'Set-NetAdapterAdvancedProperty'(see readme.md)
  Write-Host 'Patch: Disabling Set-NetAdapterAdvancedProperty'
- $updatePath= "C:\optimize\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1"
+ $updatePath= "C:\optimize\Virtual-Desktop-Optimization-Tool-master\Windows_VDOT.ps1"
  ((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
  
  # Patch: overide the REG UNLOAD, needs GC before, otherwise will Access Deny unload(see readme.md)
@@ -44,7 +44,7 @@
  
  # run script
  # .\optimize -WindowsVersion 2004 -Verbose
-  .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Verbose
+  .\Windows_VDOT.ps1 -Optimizations All -Verbose -AcceptEula
   write-host 'AIB Customization: Finished OS Optimizations script'
  
  
