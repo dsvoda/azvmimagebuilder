d43660351ff3269e0fa9180b55dcfff01d121ddc
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2023-04-01 08:10:49 -0500

    Update 0_installConfFsLogix.ps1

--- solutions/14_Building_Images_WVD/0_installConfFsLogix.ps1
+++ solutions/14_Building_Images_WVD/0_installConfFsLogix.ps1
@@ -16,5 +16,6 @@ write-host 'AIB Customization: Download Fslogix installer finished'
 
 write-host 'AIB Customization: Start Fslogix installer'
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
-.\\FSLogixSetup.ps1 -ProfilePath \\wvdSMB\wvd -Verbose 
+.\\FSLogixSetup.ps1 -ProfilePath \\wvdSMB\wvd -Verbose
+exit 0
 write-host 'AIB Customization: Finished Fslogix installer' 
